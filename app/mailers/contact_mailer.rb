e64425af13448a9f8b2189ee2bf5f0143237abf8
tomohiko <illgalsubstance@yahoo.co.jp>
2019-12-10 16:10:18 +0000

    heroku

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "tomohikohyougen@gmail.com", subject:"お問い合わせの確認目メール"
+    mail to: "illgalsubstance@yahoo.co.jp", subject:"お問い合わせの確認目メール"
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener_web
 
-end
+endconfig.consider_all_requests_local = false
